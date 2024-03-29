bee2cf76061ca05436e2d53c82db4b7c0492c31f
speedtest002 <huyprokute002@gmail.com>
2024-03-29 22:22:19 +0700

    thêm file cho hàm main của bài propeties

---
+++ testCS/propeties_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using testCS;
+
+namespace newTest
+{
+	partial class Program
+	{
+		public static void Bai1_2()
+		{
+			UserInformation user1 = new UserInformation("User 1", 28, 1001);
+			Console.WriteLine($"{user1}");
+			UserInformation user2 = new UserInformation("User 2", 24, 2002);
+			Console.WriteLine($"{user2}");
+			UserInformation user3 = new UserInformation("User 3", 43, 003);
+			Console.WriteLine($"{user3}");
+		}
+	}
+}
